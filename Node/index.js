077d93d1d5c82b176a1ea70ec8f9a812cf6d0d3f
WilliamSongLiu <williamsongliu@gmail.com>
2024-10-12 14:18:03 -0700

    Update index.js

--- Node/index.js
+++ Node/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import OpenAI from 'openai';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { setup_plot_system_prompt } from './prompts.js';
 import { storySchema } from './types.js';
 import { zodResponseFormat } from "openai/helpers/zod";
@@ -10,12 +12,17 @@ const DALL_E = 'dall-e-3';
 
 const app = express();
 const PORT = 3000;
+app.use(express.json());
 
 import apiKeys from './apiKeys.json' assert { type: 'json' };
 const openai = new OpenAI({
   apiKey: apiKeys.openai
 });
 
+app.get('/image', (req, res) => {
+  const imagePath = `./images/${req.query.file}`;
+  res.sendFile(path.join(path.dirname(fileURLToPath(import.meta.url)), imagePath));
+});
 
 app.get('/start-story', async (req, res) => {
   try {
@@ -27,7 +34,7 @@ app.get('/start-story', async (req, res) => {
     //   messages: messages,
     // });
 
-    // physical_appearance = 
+    // physical_appearance =
     // return completion.choices[0].message.content; // Access the first completion choice
     console.log(completion);
     res.send(completion);
@@ -39,7 +46,7 @@ app.get('/start-story', async (req, res) => {
   // setup
 
 
-  
+
 });
 
 const generateStorySetup = async (prompt) => {
@@ -58,13 +65,13 @@ const generateStorySetup = async (prompt) => {
       messages: messages,
       response_format: zodResponseFormat(storySchema, "story_setup"),
     });
-    
+
     return completion.choices[0].message.content;
   } catch (error) {
     console.error('Error generating completion:', error);
     throw error;
   }
-} 
+}
 
 const generatePhysicalDescription = async (prompt) => {
   try {
@@ -73,7 +80,7 @@ const generatePhysicalDescription = async (prompt) => {
       messages: messages,
       response_format: zodResponseFormat(storySchema, "story_setup"),
     });
-    
+
     // const completion = await generateCompletion(DALL_E, [
     //   { role: 'system', content: prompt },
     // ]);
@@ -95,7 +102,7 @@ const generatePhysicalDescription = async (prompt) => {
 //     throw error;
 //   }
 // }
- 
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
